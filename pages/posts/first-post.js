2dd0cd31f776ef5a10fcdaa66ee03896457c50f7
MarleyDouglas007 <marley.santos2308@gmail.com>
2023-08-17 13:57:23 -0300

    add script por terceiros

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,20 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
     <>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <Head>
+        <title>First Post</title>
+      </Head>          
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
